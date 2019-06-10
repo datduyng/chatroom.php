dd2a6ede98c9b0229cbe93bc120b3a545d6383cf
Dat Nguyen <35666615+datduyng@users.noreply.github.com>
2019-06-10 12:21:18 -0500

    Update chatroom.sql

--- chatroom.sql
+++ chatroom.sql
@@ -16,6 +16,8 @@ CREATE TABLE messages (
 );
 
 
-SELECT user_id, content, t_stamp FROM messages 
-WHERE t_stamp>='2019-6-6 8:18:5' AND t_stamp<'2019-6-6 8:18:7' 
-ORDER BY t_stamp
+-- view message between a certain period
+SELECT u.user_id, u.name as username, m.content, m.t_stamp FROM messages as m
+INNER JOIN users as u ON u.user_id=m.user_id
+WHERE t_stamp>='2019-6-7 9:44:44' AND t_stamp<'2019-6-7 9:52:52' 
+ORDER BY t_stamp;
